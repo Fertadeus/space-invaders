46d666f8f9c97f60d9d7c9fd18b8ba01f4061c0f
Fertadeus <fertadeusfp@gmail.com>
2025-02-14 10:21:21 +0100

    Menú Añadido
    Añadí un botón que te lleva al juego, y un título. Vuelve siempre al menú tras ganar. Testeado para que funcione correctamente.

--- root.gd
+++ root.gd
@@ -1,6 +1,5 @@
 extends Node
 @export var estrella: PackedScene
-var is_menu_active
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,23 +14,11 @@ func _ready() -> void:
 		var stars= estrella.instantiate()
 		stars.position=Vector2(480*randf(),720*randf())
 		add_child(stars)
-		
-	is_menu_active = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if(Input.is_action_just_pressed("Start") && is_menu_active):
-		#var menu = self.get_node("Menu")
-		#self.remove_child(menu)
-		#menu.call_deferred("free")
-		
-		var main_resource = load("res://pantalla1.tscn")
-		var main = main_resource.instantiate()
-		main.winner.connect(self._on_pantalla_1_winner)
-		self.add_child(main)
-		
-		is_menu_active=false
+	pass
 	
 
 func _on_estrella_timer_timeout() -> void:	
@@ -47,12 +34,23 @@ func _on_win_timer_timeout() -> void:
 	self.remove_child(main)
 	main.call_deferred("free")
 	
-	#var menu_resource = load("res://menu.tscn")
-	#var menu = menu_resource.instantiate()
-	#self.add_child(menu)
-	
-	is_menu_active=true
+	var menu_resource = load("res://menu.tscn")
+	var menu = menu_resource.instantiate()
+	menu.play.connect(self._on_menu_play)
+	self.add_child(menu)
+
 
 
 func _on_pantalla_1_winner() -> void:
 	$WinTimer.start()
+
+
+func _on_menu_play() -> void:
+	var menu = self.get_node("Menu")
+	self.remove_child(menu)
+	menu.call_deferred("free")
+	
+	var main_resource = load("res://pantalla1.tscn")
+	var main = main_resource.instantiate()
+	main.winner.connect(self._on_pantalla_1_winner)
+	self.add_child(main)
