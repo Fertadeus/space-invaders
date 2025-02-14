92aa81cdadb769a7e1d309d85c2dbda94cc0bc70
Fer <fertadeusfp@gmail.com>
2025-02-14 22:10:15 +0100

    Música y Warnings
    Añadida música al menú.
    Añadido efecto de disparo.
    Eliminados los warnings de distintos scripts.

--- pantalla1.gd
+++ pantalla1.gd
@@ -30,7 +30,7 @@ func _ready() -> void:
 		add_child(aliens)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
